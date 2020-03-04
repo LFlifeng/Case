a7850bce87b18d5e6f924dd9c17fe24df2101f2b
LFlfeng <2238561742@qq.com>
2020-03-04 14:57:49 +0800

    洗牌算法

---
+++ 洗牌算法.js
@@ -0,0 +1,14 @@
+//洗牌算法
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    Array.prototype.shuffle = function () {
+        let _this = this;
+        for (let i = _this.length - 1; i >= 0; i--) {
+            let randomIndex = Math.floor(Math.random() * (i + 1))
+            let itemAtIndex = _this[randomIndex];
+            _this[randomIndex] = _this[i];
+            _this[i] = itemAtIndex;
+        }
+        return _this;
+    }
+    arr.shuffle();
+    console.log(arr)
